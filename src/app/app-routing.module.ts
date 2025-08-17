716ffdceb7726f090b821e14f36c8767b4f03afb
ghost10-one <azizsokhnamboup1o@gmail.com>
2025-08-17 23:51:43 +0000

    premier commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: ' ',
+    redirectTo: 'home',
+    component: HomeComponent,
+  },
+
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+
+  {
+    path: 'details/:id',
+    component: DetailsComponent,
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+import {  HttpClientModule } from '@angular/common/http';
+import { CardModule } from 'primeng/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule ,
+    HttpClientModule ,
+    ToolbarModule ,
+    CardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
