13f30ade5e447d763c15124c67336ed369ff84b6
ghost10-one <azizsokhnamboup1o@gmail.com>
2025-08-19 13:26:26 +0000

    dashboard ecommerce profile started

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,11 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { AdminNotFoundComponent } from './pages/admin-not-found/admin-not-found.component';
+import { AdminLoginComponent } from './pages/admin-login/admin-login.component';
 
-const routes: Routes = [{ path: '', component: DashboardComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'profile',
+        pathMatch: 'full',
+      },
+      {
+        path: 'profile',
+        component: ProfileComponent,
+      },
+      {
+        path: 'login',
+        component: AdminLoginComponent,
+      },
+      {
+        path: '***',
+        component: AdminNotFoundComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class DashboardRoutingModule { }
+export class DashboardRoutingModule {}
