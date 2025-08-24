8cb95c648d7efa8581698ab47c9b3e9ba2088f1d
ghost10-one <azizsokhnamboup1o@gmail.com>
2025-08-24 16:30:56 +0000

    new auth

--- src/app/serviceProduct/auth.service.ts
+++ src/app/serviceProduct/auth.service.ts
@@ -1,11 +1,33 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';;
 
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';import { BehaviorSubject } from 'rxjs';
+;
+
+
+export interface AuthResponse {
+   accessToken : string ;
+   refreshToken : string ;
+}
+
+export interface UserPayload {
+  id : number ;
+  name : string ;
+  iat : number ;
+  exp : number ;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+
+  private readonly apiUrl = 'http://localhost:3000';
+
+  private AuthState = new BehaviorSubject<boolean | null>(null) ;
+
+  private User  =  new BehaviorSubject<UserPayload |null>(null) ;
+
+
 
 
 }
